88b4c79ee47cd7f3ca078c0c026d0ee133749be1
Smit Vora <mailsmitvora@gmail.com>
2021-10-09 09:50:31 +0530

    fix: code presentation

--- adaequare_gsp/api/gstn_ewb_api.py
+++ adaequare_gsp/api/gstn_ewb_api.py
@@ -5,24 +5,6 @@ from frappe.model.naming import make_autoname
 from adaequare_gsp.api.auth_api import AuthApi
 
 class GstnEwbApi(AuthApi):
-    FUNCTIONS = {
-        'generate_eway_bill': {
-            'action': 'GENEWAYBILL'
-        },
-        'update_vehicle_info':{
-            'action': 'VEHEWB'
-        },
-        'update_transporter':{
-            'action': 'UPDATETRANSPORTER'
-        },
-        'extend_validity':{
-            'action': 'EXTENDVALIDITY'
-        },
-        'cancel_eway_bill':{
-            'action': 'CANEWB'
-        }
-    }
-
     def __init__(self):
         super().__init__()
         self.api_name = 'enriched/ewb/'
@@ -30,9 +12,6 @@ class GstnEwbApi(AuthApi):
         self.comp_gstin = self.settings.gstin 
         self.username = self.settings.username 
         self.password = self.settings.get_password('password')
-        
-        for method_name in self.FUNCTIONS:
-            setattr(self, method_name, self.get_method(method_name))
 
     def get_eway_bill(self, ewbNo):
         response = self.make_request(
@@ -43,14 +22,27 @@ class GstnEwbApi(AuthApi):
         return response
 
     # generate post methods with different actions.
-    def get_method(self, method_name):
-        def execute(data):
-            data = json.dumps(data)
-            response = self.make_request(
-                method='post',
-                action= self.FUNCTIONS[method_name]['action'],
-                url_suffix=self.url_suffix,
-                data=data
-            )
-            return response
-        return execute
+    def make_post_request(self, action, data):
+        data = json.dumps(data)
+        response = self.make_request(
+            method='post',
+            action= action,
+            url_suffix=self.url_suffix,
+            data=data
+        )
+        return response
+
+    def generate_eway_bill(self, data):
+        return self.make_post_request('GENEWAYBILL', data)
+
+    def update_vehicle_info(self, data):
+        return self.make_post_request('VEHEWB', data)
+
+    def update_transporter(self, data):
+        return self.make_post_request('UPDATETRANSPORTER', data)
+        
+    def extend_validity(self, data):
+        return self.make_post_request('EXTENDVALIDITY', data)
+        
+    def cancel_eway_bill(self, data):
+        return self.make_post_request('CANEWB', data)
